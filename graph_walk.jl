961bf1df6aea7b7c137c43264a9d93125406cd8c
Magronox <basareh.ab@gmail.com>
2023-06-26 23:42:28 -0400

    test added

--- graph_walk.jl
+++ graph_walk.jl
@@ -54,3 +54,18 @@ function walk!(G:: DefaultDict, contig:: Vector{DNASeq}, freq, offset_in_prefix 
 
 
 end
+
+
+
+####TEST:
+## slen = 1000
+## input = randstring("ACGT",slen)
+
+## DNA_seq = string_to_DNASeq(input)
+## k=3
+## kmer_list = read_kmer(DNA_seq, length(input),k)
+
+## G = graph_creator(kmer_list,['A','C','G','T'], 5)
+## I = IS(G,['A','C','G','T'],k)
+## G_new, ~ = compact_graph(G,k,5)
+## output = run_walk(G)
