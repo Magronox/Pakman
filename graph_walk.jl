d25f05c109f9fe116615c45f4a2ded58b2532090
Magronox <basareh.ab@gmail.com>
2023-08-07 21:29:18 -0400

    Update graph_walk.jl

--- graph_walk.jl
+++ graph_walk.jl
@@ -7,6 +7,7 @@ function run_walk(G :: DefaultDict, pcontig_list:: Vector)
     begin_kmer_list = copy(keys(G))
     pid = -1
     for node in begin_kmer_list
+        print(G[node].wire_info,"\n\n")
         mn = G[node]
         for i in 1:length(mn.prefixes_terminal)
             if mn.prefixes_terminal[i] == true
@@ -31,18 +32,35 @@ function walk!(G:: DefaultDict, pcontig:: Vector{DNASeq}, freq, offset_in_prefix
     freq_rem = freq
     internal_off = 0
     off_in_wire = -1
-    
+    if length(pcontig)>(500000/64)
+        print("Fails with contig size ",(length(pcontig)-1)*64+pcontig[1].len)
+    end
+
 
     for (id,offset_in_suffix,count) in mn.wire_info[pid]
+       # print("id, ",id, "  pid, ",pid,"  offset_in_suffix ",offset_in_suffix, "  offset_inprefix ", offset_in_prefix,"  count",count,"\n")
+        
         internal_off += count
         sz = copy(count)
+        
         if internal_off + sz <= offset_in_prefix || internal_off > offset_in_prefix + freq_rem
             continue
         end
-        off_in_wire = offset_in_prefix <= internal_off ? 0 : offset_in_prefix - internal_off
-        next_off = offset_in_suffix + off_in_wire
-        freq_in_wire = min(freq_rem, sz - off_in_wire)
+        off_in_wire = offset_in_prefix <= internal_off ? 0 : offset_in_prefix - internal_off 
+        next_off = offset_in_suffix + id
+        freq_in_wire = min(freq_rem, sz - off_in_wire )
+        print("pref",DNASeq_to_string(pcontig[end]),"   suff",DNASeq_to_string(mn.suffixes[id][1]),"\n")
         contig = kmerge(pcontig, mn.suffixes[id])
+        #print(@which kmerge(pcontig, mn.suffixes[id]))
+        #assert(false)
+        #print(DNASeq_to_string(contig[en]),"\n\n")
+        for i in contig
+            print(DNASeq_to_string(i))
+        end
+        print("\n")
+        if pcontig in output
+            continue
+        end
         if mn.suffixes_terminal[id]
             push!(contig_list, contig)
             push!(output, contig)
@@ -55,15 +73,20 @@ function walk!(G:: DefaultDict, pcontig:: Vector{DNASeq}, freq, offset_in_prefix
             if !(succ_node in keys(G))
                 print("Error key not found\n")
             end
+            next_mn = G[succ_neigh(mn.label[1],mn.suffixes[id])]
+            next_prefix_id, ~ = find_succ_ext(G, mn.label, next_mn.label)
+            @assert(succ_node == next_mn.label)
+            if freq_in_wire > 0 && !(pcontig in output)
+                walk!(G, contig, freq_in_wire, next_off, next_mn, next_prefix_id, output);
+            else
+                print("error\n")
+            end 
+               
+            
         end
-
-        next_mn = G[succ_neigh(mn.label[1],mn.suffixes[id])]
-        next_prefix_id, ~ = find_succ_ext(G, mn.label, next_mn.label)
-        @assert(succ_node == next_mn.label)
-
-        walk!(G, pcontig, freq_in_wire, next_off, next_mn, next_prefix_id, output);
+       
         freq_rem -= freq_in_wire
-
+ 
 
 
     end
@@ -93,10 +116,34 @@ end
 
 ## G = graph_creator(kmer_list,['A','C','G','T'], 5)
 ## contig_list = []
-## G_new,ls = compact_graph(G,k,5)
+## G_new,ls = compact_graph!(G,3)
 ## output = run_walk(G, ls)
 
 
 
 """ contig list is defined globally, so you need to initialize it as an empty vector before running the algorithm. 
 This is because of the recursive nature of the walk algorithm."""
+
+
+slen = 200000
+input = randstring("ACGT",slen)
+
+DNA_seq = string_to_DNASeq(input)
+k=6
+kmer_list = read_kmer(DNA_seq, length(input),k)
+
+G = graph_creator(kmer_list,['A','C','G','T'], 5)
+contig_list = []
+G_new,ls = compact_graph!(G,3)
+output = run_walk(G, [])
+
+for i in contig_list
+    for j in i
+        print(DNASeq_to_string(j))
+    end
+    print("\n")
+end
+
+for i in input
+    print(i)
+end
