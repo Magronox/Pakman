f673cec779d8ff80149087e1005aaa5e295599dc
Magronox <basareh.ab@gmail.com>
2023-09-07 12:11:57 -0400

    Update read_data.jl

--- read_data.jl
+++ read_data.jl
@@ -6,29 +6,61 @@ file = "ecoli_illumina_10x_part_.fasta"
 function read_and_walk(k :: Int64, number_of_compactions :: Int64, name :: String)
     kmer_list =  DefaultDict{DNASeq, Int64}(0)
     
-    FastaReader("ecoli_illumina_10x_part_.fasta") do fr
-        
-        for (~,seq) in fr
-            input = string_to_DNASeq(seq)
-            temp_kmers = read_kmer(input, input[1].len + (length(input)-1)*64,k)
-            for(t,v) in temp_kmers
-                kmer_list[t] += v
-            end
-
-            break
+    #FastaReader(name) do fr
+    #    
+    for (~,seq) in FastaReader(name)
+        input = string_to_DNASeq(seq)
+        temp_kmers = read_kmer(input, input[1].len + (length(input)-1)*64,k)
+        for (t,v) in temp_kmers
+            kmer_list[t] += v
         end
+        break
     end
+    #end
         
     
     coverage = 5
-    contig_list = []
+    global contig_list = []
     G = graph_creator(kmer_list,['A','C','G','T'], coverage)
 
     print("Starting size ",length(G),"\n")
     G,ls = compact_graph!(G,number_of_compactions)
     run_walk(G,ls)
+    #print("\n new contig list\n",contig_list)
     contig_list, G
 end
+
+
+
+function read_and_walk_string(k :: Int64, number_of_compactions :: Int64, seq :: String)
+    kmer_list =  DefaultDict{DNASeq, Int64}(0)
+    
+    #FastaReader(name) do fr
+    #    
+    input = string_to_DNASeq(seq)
+    temp_kmers = read_kmer(input, input[1].len + (length(input)-1)*64,k)
+    for (t,v) in temp_kmers
+        kmer_list[t] += v
+    end
+    
+    
+    #end
+        
+    
+    coverage = 5
+    global contig_list = []
+    G = graph_creator(kmer_list,['A','C','G','T'], coverage)
+
+    print("Starting size ",length(G),"\n")
+    G,ls = compact_graph!(G,number_of_compactions)
+    run_walk(G,ls)
+    #print("\n new contig list\n",contig_list)
+    contig_list, G
+end
+
+
+
+
 
 
 ## Test:
@@ -36,3 +68,17 @@ end
 ## number_of_compactions = 4
 ## name = "ecoli_illumina_10x_part_.fasta"
 ## read_and_walk(k,number_of_compactions,name)
+
+## Test 
+## output, G = read_and_walk(32,28,"ecoli_illumina_10x_part_.fasta")
+## for i in output
+##    for j in i
+##        print(DNASeq_to_string(j))
+##    end
+##    print("\n\n")
+## end
+
+
+### String function Test
+## name = randstring("ACGT",1000)
+## output,G = read_and_walk_string(32,20,name)
